188f084c85e8372bce2c66babf431f4ce1109e26
Jilotage <tettoutower@gmail.com>
2025-02-25 16:45:23 +0900

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     // GitHub Pagesでホストする際のベースURLを指定（リポジトリ名に置き換え）
-    base: '/demo_js/',
+    base: process.env.NODE_ENV === 'production' ? '/demo_js/' : '/',
     plugins: [react()]
 });
